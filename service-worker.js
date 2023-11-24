f0e63e5b750bb2f6b66b6fa4c4aaf4003b1bd7c6
Sergei Cherevko <si748002@gmail.com>
2023-11-25 01:27:17 +0300

    Много фиксов активного режима

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac20436437a211f9b83fe51f95ca12f1.js"
+  "/precache-manifest.9d6ac9601dcdc82cf111727b7bd6249f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "compcon"});
